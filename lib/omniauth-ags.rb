4002b53ed470653ae377fd4169749ba8326ea95a
Timm Liu <tliu@blueskybroadcast.com>
2015-04-09 10:31:09 -0700

    Initial commit

---
+++ lib/omniauth-ags.rb
@@ -0,0 +1,8 @@
+require "omniauth-ags/version"
+require "omniauth/strategies/ags"
+
+module Omniauth
+  module Ags
+
+  end
+end
---
+++ lib/omniauth-ags/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Ags
+    VERSION = "0.0.1"
+  end
+end

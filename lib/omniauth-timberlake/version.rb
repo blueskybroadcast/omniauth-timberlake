9f52f7d97ed42204d06b6a6d4cef5959a86b7155
Elochai <buu9992@gmail.com>
2017-07-26 17:03:45 +0300

    App Events Support

--- lib/omniauth-timberlake/version.rb
+++ lib/omniauth-timberlake/version.rb
@@ -1,5 +1,5 @@
 module Omniauth
   module Timberlake
-    VERSION = "0.0.1"
+    VERSION = "0.0.2"
   end
 end
--- lib/omniauth/strategies/timberlake.rb
+++ lib/omniauth/strategies/timberlake.rb
@@ -7,6 +7,8 @@ module OmniAuth
     class Timberlake < OmniAuth::Strategies::OAuth2
       option :name, 'timberlake'
 
+      option :app_options, { app_event_id: nil }
+
       option :client_options, {
         authorize_url: 'https://staging.membershipsoftware.org/login.asp',
         api_base_url: 'https://secure005.membershipsoftware.org/stagingsecure',
@@ -29,20 +31,23 @@ module OmniAuth
       end
 
       extra do
-        { :raw_info => raw_info }
+        { raw_info: raw_info }
       end
 
       def request_phase
-        slug = session['omniauth.params']['origin'].gsub(/\//,"")
-        redirect authorize_url + "?redirectURL=" + callback_url + "?slug=#{slug}"
+        redirect authorize_url + '?redirectURL=' + callback_url + "?slug=#{account_slug}"
       end
 
       def callback_phase
+        account = Account.find_by(slug: account_slug)
+        @app_event = account.app_events.where(id: options.app_options.app_event_id).first_or_create(activity_type: 'sso')
+
         self.access_token = {
-          :token => request.params['AuthenticationToken'],
-          :token_expires => 60
+          token: request.params['AuthenticationToken'],
+          token_expires: 60
         }
         self.env['omniauth.auth'] = auth_hash
+        self.env['omniauth.app_event_id'] = @app_event.id
         call_app!
       end
 
@@ -51,7 +56,7 @@ module OmniAuth
       end
 
       def auth_hash
-        hash = AuthHash.new(:provider => name, :uid => uid)
+        hash = AuthHash.new(provider: name, uid: uid)
         hash.info = info
         hash.credentials = creds
         hash.extra = extra
@@ -64,21 +69,46 @@ module OmniAuth
 
       def validate_auth_token
         Rails.logger.error("\n==========================================\n\n #{validate_auth_url} \n\n==========================================\n")
-        response = RestClient.get(validate_auth_url)
+        request_log_text = "#{provider_name} Validate Auth Token Request:\nGET #{validate_auth_url}"
+        @app_event.logs.create(level: 'info', text: request_log_text)
+
+        begin
+          response = RestClient.get(validate_auth_url)
+        rescue RestClient::ExceptionWithResponse => e
+          create_response_error_log(e.response, e.message)
+          return
+        end
+
         parsed_response = MultiXml.parse(response)
         if response.code == 200
+          response_log_text = "#{provider_name} Validate Auth Token Response (code: #{response.code}): \n#{response}"
+          @app_event.logs.create(level: 'info', text: response_log_text)
+
           @contact_id = parsed_response['ValidateAuthenticationToken']['ValidateAuthenticationTokenResult']
           parsed_response['ValidateAuthenticationToken']['ValidateAuthenticationTokenResult']
         else
+          create_response_error_log(response)
           nil
         end
       end
 
       def get_user_info
         Rails.logger.error("\n==========================================\n\n #{user_info_url} \n\n==========================================\n")
-        response = RestClient.get(user_info_url)
+        request_log_text = "#{provider_name} Get Basic Member Info Request:\nGET #{user_info_url}"
+        @app_event.logs.create(level: 'info', text: request_log_text)
+
+        begin
+          response = RestClient.get(user_info_url)
+        rescue RestClient::ExceptionWithResponse => e
+          create_response_error_log(e.response, e.message)
+          return
+        end
+
         parsed_response = MultiXml.parse(response)
         if response.code == 200
+          response_log_text = "#{provider_name} Validate Auth Token Response (code: #{response.code}): \n#{response}"
+          @app_event.logs.create(level: 'info', text: response_log_text)
+
           info = {
             id: @contact_id,
             first_name: parsed_response['GetBasicMemberInfo']['FirstName'],
@@ -87,12 +117,30 @@ module OmniAuth
             member_type: parsed_response['GetBasicMemberInfo']['MemberType'],
             expiration_date: parsed_response['GetBasicMemberInfo']['ExpirationDate']
           }
+
+          @app_event.update(raw_data: {
+            user_info: {
+              uid: info[:id],
+              email: info[:email],
+              first_name: info[:first_name],
+              last_name: info[:last_name]
+            }
+          })
+
+          info
         else
+          create_response_error_log(response)
           nil
         end
       end
 
       private
+
+      def create_response_error_log(response, error_message = '')
+        error_log_text = "#{provider_name} Validate Auth Token Response Error #{error_message}(code: #{response&.code}):\n#{response}"
+        @app_event.logs.create(level: 'error', text: error_log_text)
+        @app_event.fail!
+      end
 
       def authorize_url
         options.client_options.authorize_url
@@ -121,6 +169,14 @@ module OmniAuth
         validate_url = options.client_options.validate_url
         validate_url += '/' if options.client_options.validate_url[-1] != '/'
         "#{base_url}#{validate_url}?securitykey=#{security_key}&token=#{access_token[:token]}"
+      end
+
+      def account_slug
+        session['omniauth.params']['origin'].gsub(/\//, '')
+      end
+
+      def provider_name
+        options.name
       end
     end
   end
